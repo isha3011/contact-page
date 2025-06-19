e1a9b2763652c97d6b7eb5bd506970bfb5950fa3
isha3011 <84734703+isha3011@users.noreply.github.com>
2025-06-20 02:26:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const animatedCards = document.querySelectorAll('.animate-from-left, .animate-from-right, .animate-from-top, .animate-from-bottom');
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('active');
-        } else {
-            entry.target.classList.remove('active');
-        }
+// Run animations only if screen width is more than 768px
+if (window.innerWidth > 768) {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            } else {
+                entry.target.classList.remove('active');
+            }
+        });
+    }, {
+        threshold: 0.2
     });
-}, {
-    threshold: 0.2
-});
 
-animatedCards.forEach(card => {
-    observer.observe(card);
-});
+    animatedCards.forEach(card => {
+        observer.observe(card);
+    });
+} else {
+    // For mobile, instantly show the cards without animation
+    animatedCards.forEach(card => {
+        card.classList.add('active');
+    });
+}
